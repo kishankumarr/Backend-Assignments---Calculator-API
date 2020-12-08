2b691a049e7e2f305402bc97d2de1c662ebcc561
kishan kumar a <kishankumar12324@gmail.com>
2020-12-08 20:36:09 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -16,33 +16,31 @@ app.get("/",(req,res)=>{
      res.status(200).send("Hello World!");
 });
  const out={
-   status:"",
-   message:"",
+   status:"success"|"failure"|"error",
+   message:"the sum of given two numbers",
  };
 
- app.post("/add", (req, res) => {
-  const num1 = req.body.num1;
-  const num2 = req.body.num2;
-  const obj = {
-    status: "success" | "failure" | "error",
-    message: "the sum of given two numbers",
-    sum: 0,
-  };
-  if (isNaN(num1) || isNaN(num2)) {
-    obj.message = "Invalid data types";
-  } else {
-    const number1 = Number(num1);
-    const number2 = Number(num2);
-    const result = number1 + number2;
-    if (number1 < -1000000 || result < -1000000) {
-      obj.message = "Underflow";
-    } else if (number1 > 1000000 || result > 1000000) {
-      obj.message = "Overflow";
-    } else {
-      obj.sum = result;
-    }
+app.post("/add", (req,res) => {
+  const num1=Number(req.body.num1);
+  const num2=Number(req.body.num2);
+
+  if(isNaN(num1)||isNaN(num2)){
+     out.message="Invalid data types";
+  }else{
+    const number1=Number(num1);
+    const number2=Number(num2);
+    const result=number1+number2;
+     if(num<-1000000||num2<-1000000||result<-1000000){
+      
+       out.message="Underflow";
+     }else if(num>1000000||num2>1000000||result>1000000){
+
+      out.message="Overflow";
+  }else{
+      out.sum=result;
   }
-  res.send(obj);
+}
+res.send(out);
 })
 
 app.post("/sub", (req,res) => {
